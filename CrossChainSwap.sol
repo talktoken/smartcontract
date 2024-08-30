49cd7a441f5f7a527155808c979521f22568a3a2
Amaan <amaansayyad@yahoo.com>
2024-08-30 14:35:47 +0530

    Update CrossChainSwap.sol

--- CrossChainSwap.sol
+++ CrossChainSwap.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract CrossChainSwap {
+    address public tokenAddress;
+
+    constructor(address _tokenAddress) {
+        tokenAddress = _tokenAddress;
+    }
+
+    function swapTokens(address recipient, uint256 amount) external {
+        IERC20(tokenAddress).transferFrom(msg.sender, recipient, amount);
+    }
+}
 
